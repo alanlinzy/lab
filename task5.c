e3fd2595dda69ca311b34d1925e4d0f443aa8600
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2019-10-19 04:20:08 +0800

    Update task5.c

--- task5.c
+++ task5.c
@@ -7,7 +7,25 @@ void main(){
 	fread(key, sizeof(unsigned char)*LEN, 1, random); 
 	fclose(random);
 	for( int i = 0; i < LEN; i++ ) 
-	{
-		printf( "%d", key[i] );
+	{   int k = key[i];
+		printBi(k);
 	}
+}
+void printBi(int k){
+	int l = sizeof(n)*8;
+    int i;
+    if(i == 0)
+    {
+         printf("0");
+         return;
+     }
+
+    for(i = l-1; i >= 0; i --)
+    {
+        if(n&(1<<i)) break;
+    }
+    for(;i>=0; i --){
+        printf("%d", (n&(1<<i)) != 0);
+
+}
 }
